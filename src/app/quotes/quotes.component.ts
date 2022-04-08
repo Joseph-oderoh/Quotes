b0f53efe098cdc65cdf28ab8364fcf6a991d1a53
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-04-08 20:39:43 +0300

    style:add css style to  quotes-component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,8 +9,9 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes: Quote [] = [
-    new Quote(1,"Joz","Open your mind", 0, 0 ),
-    new Quote(2,"Jaz","It's never to late to do better", 0, 0 ),
+    new Quote (1,'Charles','The human spirit must prevail over technology to allow humans be in charge', 0 , 0),
+    new Quote(2,"Joz","Open your mind to the world", 0, 0 ),
+    new Quote(2,"Jaz","It's never to late to do better you just havre to do it", 0, 0 ),
     new Quote(3,"Blaco","Take me back to the time we met", 0, 0 ),
     new Quote(4,"Joseph","Be you, do you,for you", 0, 0 )
   ]
