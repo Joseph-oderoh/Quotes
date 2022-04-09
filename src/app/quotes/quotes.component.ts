e822ed58060eef035e3720fbc3ebfe2b7ec7d5d1
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-04-09 09:40:51 +0300

    style:update  component-quotes array

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,10 +10,10 @@ export class QuotesComponent implements OnInit {
 
   quotes: Quote [] = [
     new Quote (1,'Noelina','The human spirit must prevail over technology to allow humans be in charge', 0 , 0),
-    new Quote(2,"Kenedy","Open your mind to the world", 0, 0 ),
+    new Quote(2,"Kenedy","Open your mind to the world experience the joy of knowlaged", 0, 0 ),
     new Quote(2,"Oderoh","It's never to late to do better you just havre to do it", 0, 0 ),
-    new Quote(3,"Blaco","Take me back to the time we met", 0, 0 ),
-    new Quote(4,"Joseph","Be you, do you,for you", 0, 0 )
+    new Quote(3,"Blaco","We are all born beautiful the worst mistake is to told  you are not ", 0, 0 ),
+    new Quote(4,"Joseph","Be you, do you,for you make yourself a priority", 0, 0 )
   ]
   toggleDetails(index: number){
     this.quotes[index].showVote = !this.quotes[index].showVote;
